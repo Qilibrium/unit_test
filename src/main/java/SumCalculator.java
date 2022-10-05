e60f44d19cd924fffeafaaa0ca5dcc2681d211f8
Oleksandr Fomin <qilibriumultra@gmail.com>
2022-10-05 21:49:22 +0300

    add all

---
+++ src/main/java/SumCalculator.java
@@ -0,0 +1,17 @@
+public class SumCalculator {
+
+    public  int sum(int n) {
+        if(n<=0){
+            throw new IllegalArgumentException("Input should be >=0");
+        }
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+
+}
+
+
---
+++ src/test/java/SumCalculatorTests.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SumCalculatorTests {
+    private SumCalculator sumCalculator;
+
+    @BeforeEach
+    public void beforeEach() {
+        sumCalculator = new SumCalculator();
+    }
+
+    @Test
+    public void testSum1InputHandledCorrectly() {
+        //When
+        int actual = sumCalculator.sum(1);
+        //Then
+        int expected = 1;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSum3InputHandledCorrectly() {
+        //When
+        int actual = sumCalculator.sum(3);
+        //Then
+        int expected = 6;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testThat0InputHandledCorrectly() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                ()->new SumCalculator().sum(0));
+    }
+}
